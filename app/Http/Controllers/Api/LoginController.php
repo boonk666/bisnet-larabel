b41875db4b72b8620c43a57c8aeb27e6d30dcad1
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-06-09 18:43:40 +0700

    fix login ap

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -7,26 +7,21 @@ use Illuminate\Http\Request;
 use App\Models\User;
 
 class LoginController extends Controller
-
 {
     public function login(Request $request){
         $user=User::whereUsername($request->input('username'))->first();
 
         if (!$user){
-            return back()->withErrors([
-                'username' => 'username yang anda masukkan salah'
-            ])->onlyInput('username');
+            return response()->json(['message' => 'username yang anda masukkan salah'], 401);
         }
 
         if (!\Hash::check($request->input('password'), $user->password)){
-            return back()->withErrors([
-                'username' => 'username yang anda masukkan salah'
-            ])->onlyInput('username');
+            return response()->json(['message' => 'password yang anda masukkan salah'], 401);
         }
                 
         return response()->json([
-            'token'=>$user->createToken('auth')
+            'token'=>$user->createToken('auth')->plainTextToken
         ]);
     }
 
-    }
+}
