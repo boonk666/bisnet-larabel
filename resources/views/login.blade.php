7d1b5b5beb639ab02cdbff173a0d984a54463b83
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-12-18 20:36:54 +0700

    login mbuh apa

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -36,7 +36,7 @@
                     <div class="card-body p-0">
                         <div class="p-5">
                             <div class="text-center">
-                                <h1 class="h4 text-gray-900 mb-4">Login</h1>
+                                <h1 class="h4 text-gray-900 mb-4">BSI.NET</h1>
                             </div>
                             <form action="{{ route('login') }}" method="POST" class="user">
                                 @csrf
