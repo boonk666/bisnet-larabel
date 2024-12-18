c79c073495e4659fd71d477375d071406f6f2a18
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-12-18 20:47:21 +0700

    laporan reset per bulan

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -9,7 +9,9 @@ class ReportController extends Controller
 {
     
     public function index() {
-        $total_payment = Payment::count();
+        $total_payment = Payment::where('status', 'success')
+            ->whereBetween('created_at', [now()->startOfMonth(), now()->endOfMonth()])
+            ->count();
 
         return view('report.index', [
             'income' => $total_payment * 85000
