6de609d03f07f2f716650cd15630855eeab534d5
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-12-18 20:35:54 +0700

    payment customer

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Payment extends Model
 {
@@ -15,20 +16,22 @@ class Payment extends Model
      * @var string
      */
     protected $table = 'pembayaran';
+
+    protected $primaryKey  = 'id_pembayaran';
     
     /**
      * fillable
      *
      * @var array
      */
-    protected $fillable = ['id_pelanggan'];
+    protected $fillable = ['id_pelanggan', 'kode_pembayaran', 'status', 'snap_url'];
     
     /**
      * pelanggan
      *
      * @return void
      */
-    public function pelanggan() {
+    public function pelanggan() : BelongsTo {
         return $this->belongsTo(Customer::class, 'id_pelanggan');
     }
 }
